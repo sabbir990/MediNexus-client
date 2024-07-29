c48c278fe77754663d1d84f27befd0fe4a41323b
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2024-07-29 08:58:44 +0100

    Completed : navbar section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'lijeva' : ['Playwrite HR Lijeva', 'cursive'],
+        'poppins' : ['Poppins', 'sans-serif'],
+        'roboto' : ['Roboto', 'sans-serif']
+      }
+    },
   },
   plugins: [
     require('daisyui'),
