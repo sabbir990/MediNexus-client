af7062be995892bae464cdbe42217b7d059498eb
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2024-07-29 08:02:56 +0100

    Added : A responsive navbar in the home page

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+import { Outlet } from 'react-router-dom'
+
+export default function Root() {
+  return (
+    <div>
+        {/* Navbar */}
+
+        <Navbar />
+
+        {/* Outlet */}
+
+        <Outlet />
+    </div>
+  )
+}
